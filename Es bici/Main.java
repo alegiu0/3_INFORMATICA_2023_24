8fae81685aac6c75cc3882b35e8be3c968e28575
3CI-GIUSTI-ALESSANDRO-2023-2024 <148719547+3CI-GIUSTI-ALESSANDRO-2023-2024@users.noreply.github.com>
2024-05-10 00:12:17 +0200

    Create Main.java

---
+++ Es bici/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * Esercitazzione con gli oop
+ * 
+ * @author (Alessandro Giusti) 
+ * @version (1)
+ */
+public class main{
+    public static void main(String[]args){
+
+    Contocorrente cont1= new Contocorrente();   
+    Bicycle bici1= new Bicycle();
+    bici1.setnpro("Paolo Rossi");
+   System.out.println(bici1);
+  
+  }
+}
